1b22e7935ccfeed70c6aa587465743e411f6373f
areceveur <adie.receveur@gmail.com>
2024-08-02 13:11:48 +0200

    Démarrage des tests en end-to-end et ajout du test sur le register

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,5 +1,5 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  it('Login successfully', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
@@ -23,5 +23,29 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  });
+
+  it('Displays error on incorrect login/password', () => {
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        error: 'Invalid credentials',
+      },
+    });
+
+    cy.get('input[formControlName=email]').type("wrong@studio.com");
+    cy.get('input[formControlName=password]').type("wrongpassword{enter}{enter}");
+
+    cy.get('.error').should('be.visible').and('contain', 'An error occurred');
+  });
+
+  it('Shows error and disable submit button if required fields are missing', () => {
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').clear();
+    cy.get('input[formControlName=password]').clear();
+
+    cy.get('button[type=submit]').should('be.disabled');
   })
 });
